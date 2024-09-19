0b83f51206bc191e0a0b9b0eca556c0ae6612d21
jonathansoriano <jonathan.s.sanjuan@outlook.com>
2024-09-18 20:56:43 -0400

    Add project files.

---
+++ InventoryMaintenance/Plant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMaintenance
+{
+    public class Plant : InvItem
+    {
+        //InvItem: int Item num, string description, decimal price 
+        public Plant() { }
+        public Plant(int itemNo, string description, decimal price, string size) :
+            base(itemNo, description, price)
+        {
+            this.Size = size;
+        }
+
+        public string Size { get; set; }
+
+        public override string GetDisplayText()
+        {
+            return $"{ItemNo} {Size} {Description} ({Price:c})";
+        }
+
+    }
+}
---
+++ InventoryMaintenance/Supply.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryMaintenance
+{
+    public class Supply : InvItem
+    {
+        public Supply() { }
+        public Supply(int intemNo, string description, decimal price, string manufacturer):
+            base(intemNo, description, price)
+        { 
+            this.Manufacturer = manufacturer;
+        }
+
+        public string Manufacturer {  get; set; }
+
+        public override string GetDisplayText()
+        {
+            return $"{ItemNo} {Manufacturer} {Description} ({Price:c})";
+        }
+
+
+        // Setter and Getter Property:
+
+        /*
+         * In C#, properties are not exactly methods, but give the power of methods. 
+         * Properties try to give the appearance of fields/attributes, but can do more than just a plain field.
+         * Getter in C#: "string name = person.Name;" or Console.WriteLine(person.Name)
+           Setter in C#: "person.Name = "John";" since we are trying to change the value of Name.
+         */
+
+        // "$" sign in C#. (Interpolation)
+        /*
+         * If you want to do interpolation (equivalent to concat (+) in java) you have to start out the string with
+         * "$" sign. If you want to include a field in the string you have to use {} brackets around the field name.
+         * No need to use (+) to include spaces. You just just use a space in the string and it spaces the strings.
+         */
+
+
+    }
+}
